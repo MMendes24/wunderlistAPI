28405f8aff3ab759ce67fb7a2558c363b3108afc
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-09-19 00:26:31 -0300

    added index server dbconfig

---
+++ server.js
@@ -0,0 +1,17 @@
+require("dotenv").config()
+const express = require("express")
+const helmet = require("helmet")
+const cors = require("cors")
+
+//const usersRouter = require("../user/users-router")
+const server = express()
+
+server.use(helmet())
+server.use(express.json())
+server.use(cors())
+
+server.get("/", (req, res) => {
+  res.json({ api: "up" })
+})
+
+module.exports = server
