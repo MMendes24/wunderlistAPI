81ab48c3a1e97728ecddf6c6fdd2f6081aceed4e
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-09-21 21:00:52 -0300

    Update 02-tasks.js

--- database/seeds/02-tasks.js
+++ database/seeds/02-tasks.js
@@ -3,12 +3,14 @@ exports.seed = function (knex) {
     {
       task: "buy groceries",
       completed: true,
-      user_id: 1
+      user_id: 1,
+      date: "12-01-2020"
     },
     {
       task: "defeat Wyoming",
       completed: false,
-      user_id: 1
+      user_id: 1,
+      date: "12-02-2020"
     }
   ])
 }
