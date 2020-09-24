0f413e09dec490d66af691617bdb91956a260a23
MMendes24 <mmbusiness1@protonmail.com>
2020-09-24 11:10:54 -0400

    Updates to taskRouter testing based on requests from my TL.

--- tasks/taskRouter.test.js
+++ tasks/taskRouter.test.js
@@ -13,19 +13,12 @@ describe('to know we can run any test we must test the tester', () => {
 })
 
 describe('taskRouter', () => {
-    
+
     afterAll(async () => {
         await db('tasks').truncate();
     })
 
     describe('GET /api/tasks', () => {
-        it('never lets us access data unless we login', () => {
-            return supertest(server)
-                .get('/api/tasks')
-                .then(res => {
-                    expect(res.status).toBe(401)
-                })
-        })
         it('requires set up due to conflict with other tests', () => {
             return supertest(server)
                 .post('/api/users/register')
@@ -55,10 +48,25 @@ describe('taskRouter', () => {
                         .set({ Authorization: token })
                         .then(res => {
                             expect(res.status).toBe(200)
-                            console.log(res.body)
                         })
                 })
         })
+        it('never lets us access data unless we login', () => {
+            return supertest(server)
+                .get('/api/tasks')
+                .then(res => {
+                    expect(res.status).toBe(401)
+                })
+        })
+        it('lets us access data because we can login', () => {
+            return supertest(server)
+                .get('/api/tasks')
+                .set({ Authorization: token })
+                .then(res => {
+                    expect(res.status).toBe(200)
+                })
+        })
+
         describe('POST /api/tasks', () => {
             it('should be able to create an item now', () => {
                 return supertest(server)
@@ -74,7 +82,64 @@ describe('taskRouter', () => {
                         expect(res.status).toBe(201)
                     })
             })
+            it('should return that item as JSON', () => {
+                return supertest(server)
+                    .post('/api/tasks')
+                    .set({ Authorization: token })
+                    .send({
+                        task: "make a test task",
+                        user_id: 1,
+                        completed: true,
+                        date: "some day"
+                    })
+                    .then(res => {
+                        expect(res.type).toMatch(/json/i)
+                    })
+            })
         })
+
+        describe('GET /api/tasks/:id', () => {
+            it('should deny us access to the club unless we have street cred', () => {
+                return supertest(server)
+                    .get('/api/tasks/1')
+                    .then(res => {
+                        expect(res.status).toBe(401)
+                    })
+            })
+            it('lets us access a unqiue task because we can login', async () => {
+                const reqTask = await db('tasks').first()
+
+                return supertest(server)
+                    .get(`/api/tasks/${reqTask.id}`)
+                    .set({ Authorization: token })
+                    .then(res => {
+                        expect(res.status).toBe(200)
+                    })
+            })
+        })
+
+        describe('GET /api/tasks/user/:id', () => {
+            it('should deny us access to the users tasks if lack cred', async () => {
+                const userId = await db('tasks').first()
+
+                return supertest(server)
+                    .get(`/api/tasks/user/${userId.user_id}`)
+                    .then(res => {
+                        expect(res.status).toBe(401)
+                    })
+            })
+            it('lets us access a unqiue users tasks because we can login', async () => {
+                const userId = await db('tasks').first()
+
+                return supertest(server)
+                    .get(`/api/tasks/user/${userId.user_id}`)
+                    .set({ Authorization: token })
+                    .then(res => {
+                        expect(res.status).toBe(200)
+                    })
+            })
+        })
+
         describe('PUT /api/tasks', () => {
             it('should be able to edit an item now', () => {
                 return supertest(server)
@@ -90,6 +155,19 @@ describe('taskRouter', () => {
                         expect(res.status).toBe(204)
                     })
             })
+            it('should fail if passed bad data', () => {
+                return supertest(server)
+                    .put("/api/tasks/1")
+                    .set({ Authorization: token })
+                    .send({
+                        task: "bad task!",
+                        completed: "how do computer?",
+                        date: "some day"
+                    })
+                    .then(res => {
+                        expect(res.status).toBe(400)
+                    })
+            })
         })
         describe('DELETE /api/tasks', () => {
             it('should be able to delete an item', () => {
@@ -98,6 +176,14 @@ describe('taskRouter', () => {
                     .set({ Authorization: token })
                     .then(res => {
                         expect(res.status).toBe(200)
+                    })
+            })
+            it('cant delete an item it cannot find', () => {
+                return supertest(server)
+                    .delete("/api/tasks/101")
+                    .set({ Authorization: token })
+                    .then(res => {
+                        expect(res.status).toBe(404)
                     })
             })
         })
